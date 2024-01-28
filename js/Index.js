ae20fce909ce6bace6aad656f6dff535bc0b83f9
Dimas Ardiminda Edia Putra <ardiminda.com>
2024-01-29 06:36:11 +0700

    revisi dikit

--- js/Index.js
+++ js/Index.js
@@ -87,7 +87,7 @@ function handleCustomQuestion(question) {
         setTimeout(() => {
             addMessage("received", "Haiiiii....");
         }, 1000); // Sesuaikan delay sesuai keinginan
-    }else {
+    } else {
         setTimeout(() => {
             addMessage("received", "Mungkin kita bisa bicarakan lewat email, silahkan hubungi saya di dimasardiminda@gmail.com");
         }, 1000); // Sesuaikan delay sesuai keinginan
@@ -132,3 +132,27 @@ floatingBtn.addEventListener('click', function () {
 closeBtn.addEventListener('click', function () {
     popupContainer.style.display = 'none';
 });
+
+// zoom
+window.onload = () => {
+    // (A) GET LIGHTBOX & ALL .ZOOMD IMAGES
+    let all = document.getElementsByClassName("zoomD"),
+        lightbox = document.getElementById("lightbox");
+
+    // (B) CLICK TO SHOW IMAGE IN LIGHTBOX
+    // * SIMPLY CLONE INTO LIGHTBOX & SHOW
+    if (all.length > 0) {
+        for (let i of all) {
+            i.onclick = () => {
+                let clone = i.cloneNode();
+                clone.className = "";
+                lightbox.innerHTML = "";
+                lightbox.appendChild(clone);
+                lightbox.className = "show";
+            };
+        }
+    }
+
+    // (C) CLICK TO CLOSE LIGHTBOX
+    lightbox.onclick = () => lightbox.className = "";
+};
